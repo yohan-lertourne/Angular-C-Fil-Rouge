ad51c9aba75a28faab9c3ac12e094f858de72c80
Thibault Montois <thibault.montois@gmail.com>
2021-12-27 16:55:19 +0100

    add components, models and services

---
+++ src/app/animated-face/animated-face.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-face',
+  templateUrl: './animated-face.component.html',
+  styleUrls: ['./animated-face.component.scss']
+})
+export class AnimatedFaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
