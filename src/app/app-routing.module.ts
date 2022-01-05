0ecc66b47ec46b839d105765aab9b75e78b2a980
ThibaultMontois <thibault.montois@gmail.com>
2022-01-05 14:39:41 +0100

    structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StoryComponent } from './main/story/story.component';
 
 const routes: Routes = [
-  { path: '', component: StoryComponent }
+
 ];
 
 @NgModule({
