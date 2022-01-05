9aa2fa5f4e6f017b81a240813ed7e1bc7cbe8f54
ThibaultMontois <thibault.montois@gmail.com>
2022-01-05 08:59:07 +0100

    structure modified // add text component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import { TimelineComponent } from './timeline/timeline.component';
-import { StoryComponent } from './story/story.component';
-import { ChoicesListComponent } from './choices-list/choices-list.component';
-import { ChoiceComponent } from './choice/choice.component';
-import { AnimatedFaceComponent } from './animated-face/animated-face.component';
-import { ContactComponent } from './contact/contact.component';
+import { TimelineComponent } from './main/timeline/timeline.component';
+import { StoryComponent } from './main/story/story.component';
+import { ChoicesListComponent } from './main/story/choices-list/choices-list.component';
+import { ChoiceComponent } from './main/story/choices-list/choice/choice.component';
+import { AnimatedFaceComponent } from './main/animated-face/animated-face.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { TextComponent } from './main/story/text/text.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ContactComponent } from './contact/contact.component';
     ChoicesListComponent,
     ChoiceComponent,
     AnimatedFaceComponent,
-    ContactComponent
+    ContactComponent,
+    TextComponent
   ],
   imports: [
     BrowserModule,
