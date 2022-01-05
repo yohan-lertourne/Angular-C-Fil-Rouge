f494309faa6402a9ae3dcfbdbbc1aca109e3fc7f
ThibaultMontois <thibault.montois@gmail.com>
2022-01-05 15:20:58 +0100

    add routes path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StepComponent } from './main/story/step/step.component';
+import { StoryComponent } from './main/story/story.component';
 
 const routes: Routes = [
-
+  { path: 'story', component: StoryComponent, children: [
+    { path: 'step:id', component: StepComponent }
+  ]}
 ];
 
 @NgModule({
