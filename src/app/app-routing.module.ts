04a3d7410c4e2b37d7c6e8eab78e5289c7b1f582
ThibaultMontois <thibault.montois@gmail.com>
2022-01-07 11:51:43 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './main/contact/contact.component';
 import { StepComponent } from './main/story/step/step.component';
 import { StoryComponent } from './main/story/story.component';
 
 const routes: Routes = [
-  { path: 'contact', component: ContactComponent },
   {
     path: 'story', component: StoryComponent, children: [
       { path: 'steps/:id', component: StepComponent }
