9ac1924dcfa23156501832e4acdd1ea7f0c4a98c
ThibaultMontois <thibault.montois@gmail.com>
2022-01-07 15:29:26 +0100

    timeline ok

--- src/app/main/timeline/timeline.component.ts
+++ src/app/main/timeline/timeline.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Step } from 'src/app/models/step.model';
 import { StepsService } from 'src/app/services/steps.service';
 
@@ -9,6 +10,9 @@ import { StepsService } from 'src/app/services/steps.service';
 })
 export class TimelineComponent implements OnInit {
 
+  id!: number;
+  step!: Step;
+
   start_time!: number;
   total_duration!: number;
 
@@ -17,18 +21,25 @@ export class TimelineComponent implements OnInit {
 
   myObserver = {
     next: ((id: number) => {
-      this.changeIconPosition(id);
-      this.loadIcon(id, id !== 1);
+      this.id = id;
+      this.step = this.stepsservice.getStepById(id) as Step;
+      this.changeIconPosition();
+      this.loadIcon(id !== 1);
     })
   };
 
-  constructor(private stepsservice: StepsService) { }
+  constructor(private stepsservice: StepsService, private activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.setInitialTimes();
     this.timeline = document.getElementsByClassName('timeline')[0] as HTMLElement;
-    this.timeline_icon = document.getElementsByClassName("timeline_icon")[0] as HTMLImageElement;
+    this.timeline_icon = document.getElementsByClassName('timeline_icon')[0] as HTMLImageElement;
     this.stepsservice.subject.subscribe(this.myObserver);
+    window.addEventListener('resize', () => {
+      this.timeline_icon.classList.add('timeline_icon_window_resize');
+      this.changeIconPosition();
+      setTimeout(() => this.timeline_icon.classList.remove('timeline_icon_window_resize'), 100);
+    });
   }
 
   parseTime(str: string): number {
@@ -44,9 +55,8 @@ export class TimelineComponent implements OnInit {
     this.total_duration = this.parseTime(`${fin.getHours()}:${fin.getMinutes()}`) - this.start_time;
   }
 
-  loadIcon(id: number, is_fading: boolean): void {
-    let step = this.stepsservice.getStepById(id) as Step;
-    let icon = step.theme.icon;
+  loadIcon(is_fading: boolean): void {
+    let icon = this.step.theme.icon;
     this.timeline_icon.setAttribute('alt', icon.alt);
     this.timeline_icon.setAttribute('author', icon.author);
     if (is_fading) {
@@ -57,9 +67,8 @@ export class TimelineComponent implements OnInit {
     else this.timeline_icon.setAttribute('src', icon.src);
   }
 
-  changeIconPosition(id: number): void {
-    let step = this.stepsservice.getStepById(id) as Step;
-    let time = this.parseTime(`${step.hour.getHours()}:${step.hour.getMinutes()}`) - this.start_time;
+  changeIconPosition(): void {
+    let time = this.parseTime(`${this.step.hour.getHours()}:${this.step.hour.getMinutes()}`) - this.start_time;
     let icon_position = (this.timeline.offsetWidth - this.timeline_icon.offsetWidth - parseInt(getComputedStyle(this.timeline).padding.split('px')[0]) * 2) * (time / this.total_duration);
     this.timeline_icon.setAttribute('style', `transform: translate(${icon_position}px)`);
   }
