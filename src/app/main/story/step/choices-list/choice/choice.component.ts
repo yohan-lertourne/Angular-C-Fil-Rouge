246778f42700e64918da751189edfead315b9311
yohan-lertourne <yohan.lertourne@gmail.com>
2022-01-07 11:13:40 +0100

    function test on choice

--- src/app/main/story/step/choices-list/choice/choice.component.ts
+++ src/app/main/story/step/choices-list/choice/choice.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Step } from 'src/app/models/step.model';
+import { StepsService } from 'src/app/services/steps.service';
 
 @Component({
   selector: 'app-choice',
@@ -8,10 +11,18 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ChoiceComponent implements OnInit {
   @Input() text!: string;
 
-  constructor() { }
+  step!: Step;
+  
+  constructor(private StepsService: StepsService,private router: Router) { }
 
   ngOnInit(): void {
-
+    this.step = this.StepsService.getStepById(1) as Step;
   }
 
+  public test() {
+    this.step.id = this.step.id + 1;
+    this.StepsService.subject.next(this.step.id);
+    this.router.navigate(['/story/steps/'+ this.step.id]);
+    console.log("step" + this.step.id)
+  }
 }
