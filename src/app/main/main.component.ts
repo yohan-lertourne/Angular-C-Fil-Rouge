6111e79c1a8a4b908d99c450e6e124ead1f9c7c6
ThibaultMontois <thibault.montois@gmail.com>
2022-01-12 15:29:06 +0100

    Update main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { Step } from '../models/step.model';
 import { StepsService } from '../services/steps.service';
 
 @Component({
